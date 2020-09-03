6776c36e36cdd503d01b62f3875a7f4be803db4a
Kadir Ince <kadirincedev@gmail.com>
2020-09-03 14:33:40 +0300

    Initial Commit

---
+++ GameUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameUI
+//
+//  Created by Kadir on 3.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GameUI/GameUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameUIApp.swift
+//  GameUI
+//
+//  Created by Kadir on 3.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct GameUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
