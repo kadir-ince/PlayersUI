64ea1843a8fe47903782a29d6bafdd96fb73fce7
Kadir Ince <kadirincedev@gmail.com>
2020-09-03 14:46:33 +0300

    Added Gradient Color & Icon for Home View

--- GameUI/ContentView.swift
+++ GameUI/ContentView.swift
@@ -9,13 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Home()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct Home: View {
+    var body: some View {
+        VStack{
+            HStack {
+                Text("Your Player")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                
+                Spacer()
+                Button(action: {
+                    
+                }) {
+                    Image(systemName: "line.horizontal.3.decrease")
+                        .font(.title)
+                        .foregroundColor(.white)
+                }
+            }
+            .padding(.horizontal)
+            .padding(.top)
+            
+            Spacer()
+        }
+        .background(LinearGradient(gradient: .init(colors: [Color("Color"),Color("Color1")]), startPoint: .top, endPoint: .bottom).ignoresSafeArea(.all))
     }
 }
